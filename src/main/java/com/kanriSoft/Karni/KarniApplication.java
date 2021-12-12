2c98f4877f3ac2776358615cd053ba83c851c3a5
ravi <ravi@webbeeglobal.com>
2021-12-12 18:44:53 +0530

    add projecty

---
+++ src/main/java/com/kanriSoft/Karni/KarniApplication.java
@@ -0,0 +1,14 @@
+package com.kanriSoft.Karni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarniApplication {
+
+	public static void main(String[] args) {
+		System.out.println("First Step ");
+		SpringApplication.run(KarniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kanriSoft/Karni/KarniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanriSoft.Karni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
