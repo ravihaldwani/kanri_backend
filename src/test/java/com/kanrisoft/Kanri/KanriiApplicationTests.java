3454becb33f3c90722c39d29a3bf737921861e2d
ravi <ravi@webbeeglobal.com>
2021-12-12 19:15:57 +0530

    changes

--- src/test/java/com/kanrisoft/Kanri/KanriiApplicationTests.java
+++ src/test/java/com/kanrisoft/Kanri/KanriiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.kanrisoft.Karni;
+package com.kanrisoft.Kanri;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KarniApplicationTests {
+class KanriApplicationTests {
 
 	@Test
 	void contextLoads() {
