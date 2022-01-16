f0d5ff1f3ff376ee2fbe9237e3df00401730af8b
Ankit Sharma <sankit72@gmail.com>
2022-01-16 12:34:47 +0530

    refactored user api

--- src/main/java/com/kanrisoft/kanri/user/UserController.java
+++ src/main/java/com/kanrisoft/kanri/user/UserController.java
@@ -1,12 +1,9 @@
 package com.kanrisoft.kanri.user;
 
-import com.kanrisoft.kanri.user.exception.InvalidRequestException;
-import com.kanrisoft.kanri.user.model.LoginRequest;
-import com.kanrisoft.kanri.user.model.RegisterRequest;
+import com.kanrisoft.kanri.user.model.RegistrationRequest;
 import com.kanrisoft.kanri.user.model.UserDto;
 import com.kanrisoft.kanri.user.service.UserService;
 import com.kanrisoft.kanri.user.util.UserUtils;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,41 +12,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/user")
 class UserController {
     private final UserService userService;
-    private final LoginService loginService;
 
-    UserController(
-            UserService userService,
-            LoginService loginService
-    ) {
+    UserController(UserService userService) {
         this.userService = userService;
-        this.loginService = loginService;
     }
 
     @PostMapping("/register")
-    ResponseEntity<Object> register(@RequestBody RegisterRequest request) {
-        try {
-            var user = userService.register(request);
-            return ResponseEntity.status(HttpStatus.CREATED).body(UserUtils.mapUserToDto(user));
-        } catch (InvalidRequestException exp) {
-            return ResponseEntity.badRequest().body(exp);
-        }
+    ResponseEntity<UserDto> register(@RequestBody RegistrationRequest request) {
+        var user = userService.register(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserUtils.mapUserToDto(user));
     }
 
-    @PostMapping(value = "/login")
-    public ResponseEntity<Object> generateToken(@RequestBody LoginRequest request) {
-        var token = loginService.login(request);
-
-        return ResponseEntity.ok()
-                .header(
-                        HttpHeaders.AUTHORIZATION,
-                        token
-                ).body("Logged in successfully");
-    }
-
-    @GetMapping("/me")
+    @GetMapping
     public ResponseEntity<UserDto> getMe() {
         var user = userService.getCurrentUser();
         return ResponseEntity.ok(UserUtils.mapUserToDto(user.get()));
+    }
+
+    @PutMapping
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto request) {
+        var user = userService.updateUser(request);
+        return ResponseEntity.ok(UserUtils.mapUserToDto(user));
     }
 
     @GetMapping("/activate")
@@ -57,11 +40,5 @@ class UserController {
         userService.activateUser(key);
         // send a redirect to our home page
         return ResponseEntity.ok("User activated");
-    }
-
-    @PostMapping("/update")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto request) {
-        var user = userService.updateUser(request);
-        return ResponseEntity.ok(UserUtils.mapUserToDto(user));
     }
 }
