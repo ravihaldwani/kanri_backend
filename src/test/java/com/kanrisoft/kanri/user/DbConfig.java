ccb770937940c09fb6485d86f27d83ea942ca10a
Ankit Sharma <sankit72@gmail.com>
2022-01-21 21:13:41 +0530

    added config for test

---
+++ src/test/java/com/kanrisoft/kanri/user/DbConfig.java
@@ -0,0 +1,40 @@
+package com.kanrisoft.kanri.user;
+
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test")
+@DataJdbcTest()
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(initializers = DbConfig.DockerPostgreDataSourceInitializer.class)
+@Testcontainers
+public abstract class DbConfig {
+
+    public static PostgreSQLContainer<?> postgreDBContainer = new PostgreSQLContainer<>("postgres:13.4")
+            .withInitScript("schema.sql");
+
+    static {
+        postgreDBContainer.start();
+    }
+
+    public static class DockerPostgreDataSourceInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+
+            TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+                    applicationContext,
+                    "spring.datasource.url=" + postgreDBContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + postgreDBContainer.getUsername(),
+                    "spring.datasource.password=" + postgreDBContainer.getPassword()
+            );
+        }
+    }
+}
